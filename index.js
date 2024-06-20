a8879b9be0a19bad05f96a67edbc2b3fd5d19671
eddie bautista <ebautistab@uniguajira.edu.co>
2024-06-20 15:16:33 -0500

    inicial

---
+++ index.js
@@ -0,0 +1,41 @@
+  require('elastic-apm-node').start();
+
+  const { ApolloServer, gql } = require('apollo-server');
+
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+    
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+  const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
